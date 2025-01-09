eb669d02539886be153b029eb686f175790572ec
DibsTHEgreat <pateliyandivya@gmail.com>
2025-01-09 11:41:38 -0700

    progress

---
+++ ServerManagement/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerManagement.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
